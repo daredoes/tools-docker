3e20df97fe5382db2d5c7e000e7680fba8eefb8f
Daniel Evans <me@daredoes.work>
2021-03-16 23:41:36 -0700

    update admin item model

--- src/myinventory/admin.py
+++ src/myinventory/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import ItemModel
 
+class ItemModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id', 'image')
+    list_display_links = ('name', 'id')
+
 admin.site.register(ItemModel)
